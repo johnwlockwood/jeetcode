1cf2208a91921a7c2c52d2503125cdbad66fa0e5
John.Lockwood <johnwlockwood@gmail.com>
2019-12-16 09:10:51 -0800

    Add a naive solution

---
+++ cmd/happyNumber.go
@@ -0,0 +1,103 @@
+/*
+Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+
+	"github.com/spf13/cobra"
+)
+
+// happyNumberCmd represents the happyNumber command
+var happyNumberCmd = &cobra.Command{
+	Use:   "happyNumber",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("happyNumber called")
+		theDigits := digits(934935)
+		if got, want := theDigits, []int{9, 1}; !reflect.DeepEqual(got, want) {
+			fmt.Printf("got %v, want %v\n", got, want)
+		} else {
+			fmt.Printf("success: got %v, want %v\n", got, want)
+		}
+		if got, want := isHappy(934935), true; got != want {
+			fmt.Printf("got %v, want true\n", got)
+		} else {
+			fmt.Printf("success: got %v, want %v\n", got, want)
+		}
+	},
+}
+
+func isHappy(n int) bool {
+	// The nth digit is (the remainder of dividing by 10n) divided by 10n-1
+	if n < 0 {
+		return false
+	}
+	seen := make(map[int]struct{})
+	for n != 1 {
+		sum := 0
+		theDigits := digits(n)
+		fmt.Println("digits of ", n, " are ", theDigits)
+		squares := make([]int, 0, len(theDigits))
+		for _, v := range theDigits {
+			sq := int(math.Pow(float64(v), float64(2)))
+			squares = append(squares, sq)
+			sum = sum + sq
+		}
+		fmt.Println("sum of squares of ", theDigits, " is ", sum, " squares: ", squares)
+		if _, ok := seen[sum]; ok {
+			break
+		}
+		seen[sum] = struct{}{}
+		n = sum
+	}
+	return n == 1
+}
+
+func digits(n int) []int {
+	values := make([]int, 0)
+	if n < 0 {
+		return values
+	}
+
+	for i := 0; int(math.Pow10(i)) <= n; i++ {
+		dig := (n % int(math.Pow10(i+1))) / int(math.Pow10(i))
+		values = append(values, dig)
+	}
+	return values
+}
+
+func init() {
+	rootCmd.AddCommand(happyNumberCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// happyNumberCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// happyNumberCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
