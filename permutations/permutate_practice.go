26e1a5f08d9fb29f3c636a664e48bbdccc6530b6
John.Lockwood <johnwlockwood@gmail.com>
2019-12-17 11:32:06 -0800

    Add practice from blank

--- permutations/permutate_practice.go
+++ permutations/permutate_practice.go
@@ -2,12 +2,11 @@ package permutations
 
 func backtrackPractice(n, first int, nums []int, perms *[][]int) {
 	if first == n {
-		// output nums
 		numsCopy := make([]int, len(nums))
 		copy(numsCopy, nums)
 		*perms = append(*perms, numsCopy)
 	} else {
-		// iterate from first to n-1, swap first and i, backtrack and unswap
+		// iterate from first to n-1, swap nums first and i, backtrack with first +1, unswap nums
 		for i := first; i < n; i++ {
 			nums[first], nums[i] = nums[i], nums[first]
 			backtrackPractice(n, first+1, nums, perms)
@@ -17,11 +16,8 @@ func backtrackPractice(n, first int, nums []int, perms *[][]int) {
 }
 
 func permuteBacktrackPractice(nums []int) [][]int {
-	// initialize perms matrix
+	// init perms matrix
 	perms := make([][]int, 0)
-	n := len(nums)
-	nums1st := make([]int, n)
-	copy(nums1st, nums)
-	backtrackPractice(n, 0, nums1st, &perms)
+	backtrackPractice(len(nums), 0, nums, &perms)
 	return perms
 }
