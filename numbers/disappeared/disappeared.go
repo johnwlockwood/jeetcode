b4efc8e2c35b6a588f1a90ac2a11e0f959a4616d
John.Lockwood <johnwlockwood@gmail.com>
2019-12-19 11:01:47 -0800

    Use std lib math.Abs to get index making it faster
    Use original array to store output

--- numbers/disappeared/disappeared.go
+++ numbers/disappeared/disappeared.go
@@ -1,27 +1,30 @@
 package disappeared
 
+import "math"
+
 func findDisappearedNumbers(nums []int) []int {
 	// the index is a place to store information
 	// if the value is negative, it is marked as seen at that index
 	// so any positive values indicate its index has not been marked
-	d := make([]int, 0)
 	// for each number, mark that number by negating the number at it's index
 	for _, v := range nums {
-		if v < 0 {
-			if nums[-1*v-1] > 0 {
-				nums[-1*v-1] *= -1
-			}
-		} else {
-			if nums[v-1] > 0 {
-				nums[v-1] *= -1
-			}
+		vi := int(math.Abs(float64(v))) - 1
+		if nums[vi] > 0 {
+			nums[vi] *= -1
 		}
 	}
-	// if the number in i is positive, add i+1 to d
+	// use the original array to record the values
+	// the value at an index already looked at doesn't need to be
+	// kept, so free to overwrite it.
+	// if the number in i is positive.
+	// record it at the next available place
+	var outI int
 	for i, v := range nums {
 		if v > 0 {
-			d = append(d, i+1)
+			nums[outI] = i + 1
+			outI++
 		}
 	}
-	return d
+	// return only the part of the array with the disappeared values
+	return nums[:outI]
 }
