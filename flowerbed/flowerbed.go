021de65156ac5e7277a9a224327ce888c6380296
John.Lockwood <johnwlockwood@gmail.com>
2019-12-20 13:27:00 -0800

    Naive flowerbed solution

---
+++ flowerbed/flowerbed.go
@@ -0,0 +1,54 @@
+package flowerbed
+
+import "fmt"
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	// mark existing flowers as -1
+	// plant flowers in even spots which are >0
+	// for every -1 spot, set the surrounding spots to 0 if they are in the flowerbed
+	// plant the last plot if the final two are empty
+	// count positive values
+	fmt.Printf("original flowerbed \t\t\t%v \t%d free?\n", flowerbed, n)
+	freePlots := 0
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == 1 {
+			flowerbed[i] = -1
+		}
+	}
+
+	fmt.Printf("\tmarked flowerbed \t\t%v\n", flowerbed)
+	for i := 0; i < len(flowerbed); i++ {
+		if i%2 == 0 && flowerbed[i] >= 0 {
+			flowerbed[i] = 1
+		}
+	}
+	fmt.Printf("\tproposed dirty flowerbed \t%v\n", flowerbed)
+
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == -1 {
+			if i < len(flowerbed)-1 && flowerbed[i+1] == 1 {
+				flowerbed[i+1] = 0
+			}
+			if i > 0 && flowerbed[i-1] == 1 {
+				flowerbed[i-1] = 0
+			}
+		}
+	}
+	fmt.Printf("\tcleaned flowerbed \t\t%v\n", flowerbed)
+
+	// try to plant the last plot
+	if len(flowerbed) > 1 {
+		if flowerbed[len(flowerbed)-1] == 0 && flowerbed[len(flowerbed)-2] == 0 {
+			flowerbed[len(flowerbed)-1] = 1
+		}
+	}
+	fmt.Printf("\tlast plot planted flowerbed \t%v\n", flowerbed)
+
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] > 0 {
+			freePlots++
+		}
+	}
+	fmt.Printf("\t\t\t\t%d free\n", freePlots)
+	return n <= freePlots
+}
