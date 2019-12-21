bcdf2e12fd0880c922bdea6d537c402547dd5790
John.Lockwood <johnwlockwood@gmail.com>
2019-12-20 16:06:54 -0800

    Add merge tree approach 1

--- bintree/merge/merge_test.go
+++ bintree/merge/merge_test.go
@@ -142,3 +142,140 @@ func TestMerge(t *testing.T) {
 
 	}
 }
+
+func TestMergeApproach1(t *testing.T) {
+	type test struct {
+		name   string
+		inputS *bintree.TreeNode
+		inputT *bintree.TreeNode
+		want   *bintree.TreeNode
+	}
+	tests := []test{
+		{
+			name: "[1 1][1]",
+			inputS: &bintree.TreeNode{
+				Val: 1,
+				Left: &bintree.TreeNode{
+					Val: 1,
+				},
+			},
+			inputT: &bintree.TreeNode{
+				Val: 1,
+			},
+			want: &bintree.TreeNode{
+				Val: 2,
+				Left: &bintree.TreeNode{
+					Val: 1,
+				},
+			},
+		},
+		{
+			name: "[4 1][4 1]",
+			inputS: &bintree.TreeNode{
+				Val: 4,
+				Left: &bintree.TreeNode{
+					Val: 1,
+				},
+			},
+			inputT: &bintree.TreeNode{
+				Val: 4,
+				Left: &bintree.TreeNode{
+					Val: 1,
+				},
+			},
+			want: &bintree.TreeNode{
+				Val: 8,
+				Left: &bintree.TreeNode{
+					Val: 2,
+				},
+			},
+		},
+		{
+			name: "[4 3 null 1][3 null 1]",
+			inputS: &bintree.TreeNode{
+				Val: 4,
+				Left: &bintree.TreeNode{
+					Val: 3,
+					Right: &bintree.TreeNode{
+						Val: 1,
+					},
+				},
+			},
+			inputT: &bintree.TreeNode{
+				Val: 3,
+				Right: &bintree.TreeNode{
+					Val: 1,
+				},
+			},
+			want: &bintree.TreeNode{
+				Val: 7,
+				Left: &bintree.TreeNode{
+					Val: 3,
+					Right: &bintree.TreeNode{
+						Val: 1,
+					},
+				},
+				Right: &bintree.TreeNode{
+					Val: 1,
+				},
+			},
+		},
+		{
+			name: `[3,4,5,1,2,null,null,0][4,1,2]`,
+			inputS: &bintree.TreeNode{
+				Val: 3,
+				Left: &bintree.TreeNode{
+					Val: 4,
+					Left: &bintree.TreeNode{
+						Val: 1,
+						Left: &bintree.TreeNode{
+							Val: 0,
+						},
+					},
+					Right: &bintree.TreeNode{
+						Val: 2,
+					},
+				},
+				Right: &bintree.TreeNode{
+					Val: 5,
+				},
+			},
+			inputT: &bintree.TreeNode{
+				Val: 4,
+				Left: &bintree.TreeNode{
+					Val: 1,
+				},
+				Right: &bintree.TreeNode{
+					Val: 2,
+				},
+			},
+			want: &bintree.TreeNode{
+				Val: 7,
+				Left: &bintree.TreeNode{
+					Val: 5,
+					Left: &bintree.TreeNode{
+						Val: 1,
+						Left: &bintree.TreeNode{
+							Val: 0,
+						},
+					},
+					Right: &bintree.TreeNode{
+						Val: 2,
+					},
+				},
+				Right: &bintree.TreeNode{
+					Val: 7,
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := mergeTreesApproach1(tc.inputS, tc.inputT); bintree.PreOrder(got) != bintree.PreOrder(tc.want) {
+				t.Errorf("got %v, want %v", bintree.PreOrder(got), bintree.PreOrder(tc.want))
+			}
+		})
+
+	}
+}
