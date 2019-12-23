4f74ce1158357a743ccd5d9dccaeabdd24c2fbcc
John.Lockwood <johnwlockwood@gmail.com>
2019-12-23 11:11:18 -0800

    Revert "Make simpler"
    This reverts commit 8c5b20f9f1595345d219e80fbb88215cbfa6c98e.

--- strings/permutations/phone/phone.go
+++ strings/permutations/phone/phone.go
@@ -1,7 +1,5 @@
 package phone
 
-import "strings"
-
 // Runtime: 0 ms, faster than 100.00% of Go online submissions for Letter Combinations of a Phone Number.
 // Memory Usage: 2.5 MB, less than 54.55% of Go online submissions for Letter Combinations of a Phone Number.
 // https://leetcode.com/submissions/detail/288038011/
@@ -21,17 +19,25 @@ func letterCombinations(digits string) []string {
 	}
 	lastDigit := digits[len(digits)-1]
 	digitLetters := digitToLetters[lastDigit]
-	lastCombos := strings.Split(digitLetters, "")
+	lastCombos := make([]string, 0, len(digitLetters))
+
+	for _, d := range digitLetters {
+		lastCombos = append(lastCombos, string(d))
+	}
+	if len(digits) == 1 {
+		return lastCombos
+	}
 	for i := len(digits) - 2; i >= 0; i-- {
 		d := digits[i]
-		letters := strings.Split(digitToLetters[d], "")
+		letters := digitToLetters[d]
 		combos := make([]string, 0)
 		for _, prefix := range letters {
 			for _, suffix := range lastCombos {
-				combos = append(combos, prefix+suffix)
+				combos = append(combos, string(prefix)+suffix)
 			}
 		}
 		lastCombos = combos
 	}
+
 	return lastCombos
 }
