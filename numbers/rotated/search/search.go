e669a3d00da7337b6bd1a6fb17a04496eef7af1a
John.Lockwood <johnwlockwood@gmail.com>
2019-12-24 13:39:19 -0800

    Add passing search in rotated array

--- numbers/rotated/search/search.go
+++ numbers/rotated/search/search.go
@@ -2,8 +2,8 @@ package search
 
 // find the rotation point
 func findRotation(rotated []int, left, right int) int {
-	if right == left {
-		return right
+	if right <= left {
+		return left
 	}
 	half := ((right + 1 - left) / 2) + left
 	if rotated[half-1] > rotated[half] {
@@ -40,15 +40,21 @@ func binarySearch(nums []int, item, low, high int) int {
 	return binarySearch(nums, item, low, mid-1)
 }
 
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Search in Rotated Sorted Array.
+// Memory Usage: 2.6 MB, less than 50.00% of Go online submissions for Search in Rotated Sorted Array.
+// https://leetcode.com/submissions/detail/288307651/
 func search(nums []int, target int) int {
 	// find rotation index with binary like search
 	// find value with binary search
+	if len(nums) <= 0 {
+		return -1
+	}
 	pivot := findRotation(nums, 0, len(nums)-1)
 	if nums[pivot] == target {
 		return pivot
-	} else if nums[pivot] > target && pivot > 0 {
+	} else if pivot > 0 && nums[pivot-1] >= target && target > nums[len(nums)-1] {
 		// search left of pivot
-		return binarySearch(nums, target, 0, pivot-1)
+		return binarySearch(nums, target, 0, pivot)
 	}
 	// search right of pivot
 	return binarySearch(nums, target, pivot, len(nums)-1)
--- numbers/rotated/search/search_test.go
+++ numbers/rotated/search/search_test.go
@@ -30,6 +30,11 @@ func TestFindRotation(t *testing.T) {
 			want:  0,
 		},
 		{
+			name:  "size 0",
+			input: []int{},
+			want:  0,
+		},
+		{
 			name:  "pivot at zero of odd",
 			input: []int{0, 1, 2},
 			want:  0,
@@ -56,25 +61,55 @@ func TestFindRotation(t *testing.T) {
 
 func TestSearch(t *testing.T) {
 	type test struct {
+		name   string
 		input  []int
 		inputT int
 		want   int
 	}
 	tests := []test{
 		{
+			name:   "[5,1,3] 3",
+			input:  []int{5, 1, 3},
+			inputT: 3,
+			want:   2,
+		},
+		{
+			name:   "0 in 1 pivot 0",
+			input:  []int{1},
+			inputT: 0,
+			want:   -1,
+		},
+		{
+			name:   "3 in 2 pivot 1",
+			input:  []int{3, 1},
+			inputT: 3,
+			want:   0,
+		},
+		{
+			name:   "4 in 7 pivot 4",
 			input:  []int{4, 5, 6, 7, 0, 1, 2},
 			inputT: 0,
 			want:   4,
 		},
 		{
+			name:   "3 not in 7 pivot 4",
 			input:  []int{4, 5, 6, 7, 0, 1, 2},
+			inputT: 3,
+			want:   -1,
+		},
+		{
+			name:   "3 not in 0, no pivot",
+			input:  []int{},
 			inputT: 3,
 			want:   -1,
 		},
 	}
 	for _, tc := range tests {
-		if got := search(tc.input, tc.inputT); got != tc.want {
-			t.Errorf("searching %v for %d, got %d, want %d", tc.input, tc.inputT, got, tc.want)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			if got := search(tc.input, tc.inputT); got != tc.want {
+				t.Errorf("searching %v for %d, got %d, want %d", tc.input, tc.inputT, got, tc.want)
+			}
+		})
+
 	}
 }
