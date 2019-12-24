a2d5d0f4dc384ce3a1430ac82b525649f543b108
John.Lockwood <johnwlockwood@gmail.com>
2019-12-24 12:10:49 -0800

    Add working rotation finder

--- numbers/rotated/search/search.go
+++ numbers/rotated/search/search.go
@@ -1,5 +1,34 @@
 package search
 
+// find the rotation point
+func findRotation(rotated []int, left, right int) int {
+	if right < left {
+		return -1
+	}
+	if right == left {
+		return right
+	}
+	half := ((right + 1 - left) / 2) + left
+	if rotated[left] > rotated[right] {
+		if right-left == 2 {
+			return right
+		}
+		if rotated[left] > rotated[half] {
+			if half-left == 1 {
+				return half
+			}
+			// search left
+			return findRotation(rotated, left, half-1)
+		}
+		// search right
+		return findRotation(rotated, half, right)
+
+	}
+	return left
+}
 func search(nums []int, target int) int {
+	// find rotation index with binary like search
+	// find value with binary search
+
 	return -1
 }
--- numbers/rotated/search/search_test.go
+++ numbers/rotated/search/search_test.go
@@ -2,6 +2,29 @@ package search
 
 import "testing"
 
+func TestFindRotation(t *testing.T) {
+	type test struct {
+		input  []int
+		inputT int
+		want   int
+	}
+	tests := []test{
+		{
+			input: []int{4, 5, 6, 7, 0, 1, 2},
+			want:  4,
+		},
+		{
+			input: []int{4, 5, 6, 7, 8, 0, 1, 2},
+			want:  5,
+		},
+	}
+	for _, tc := range tests {
+		if got := findRotation(tc.input, 0, len(tc.input)-1); got != tc.want {
+			t.Errorf("searching %v for %d, got %d, want %d", tc.input, tc.inputT, got, tc.want)
+		}
+	}
+}
+
 func TestSearch(t *testing.T) {
 	type test struct {
 		input  []int
