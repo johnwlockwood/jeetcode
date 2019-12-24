448878b7bf94271ec34d3b013261cf1385008732
John.Lockwood <johnwlockwood@gmail.com>
2019-12-24 12:59:21 -0800

    Fix binary search

--- numbers/rotated/search/search.go
+++ numbers/rotated/search/search.go
@@ -2,12 +2,10 @@ package search
 
 // find the rotation point
 func findRotation(rotated []int, left, right int) int {
-	if right < left {
-		return -1
-	}
 	if right == left {
 		return right
 	}
+	// mid := (left + right) / 2
 	half := ((right + 1 - left) / 2) + left
 	if rotated[half-1] > rotated[half] {
 		// pivot is in the middle
@@ -24,9 +22,34 @@ func findRotation(rotated []int, left, right int) int {
 	}
 	return left
 }
+
+func binarySearch(nums []int, item, low, high int) int {
+	if high <= low {
+		if item == nums[low] {
+			return low
+		}
+		return -1
+	}
+	mid := (low + high) / 2
+
+	if item == nums[mid] {
+		return mid
+	}
+	if item > nums[mid] {
+		return binarySearch(nums, item, mid+1, high)
+	}
+	return binarySearch(nums, item, low, mid-1)
+}
+
 func search(nums []int, target int) int {
 	// find rotation index with binary like search
 	// find value with binary search
-
-	return -1
+	pivot := findRotation(nums, 0, len(nums)-1)
+	if nums[pivot] == target {
+		return pivot
+	} else if nums[pivot] > target && pivot > 0 {
+		// search left of pivot
+		return binarySearch(nums, target, 0, pivot-1)
+	}
+	return binarySearch(nums, target, pivot, len(nums)-1)
 }
