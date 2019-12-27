fb4bde359afce812d5cc0795d66f9a0ab0c5e453
John.Lockwood <johnwlockwood@gmail.com>
2019-12-27 10:05:31 -0800

    Add test stub for merge

---
+++ numbers/intervals/merge/merge_test.go
@@ -0,0 +1,29 @@
+package merge
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	type test struct {
+		input [][]int
+		want  [][]int
+	}
+
+	tests := []test{
+		{
+			input: [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+			want:  [][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			input: [][]int{{1, 4}, {4, 5}},
+			want:  [][]int{{1, 5}},
+		},
+	}
+	for _, tc := range tests {
+		if got := merge(tc.input); !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("%v got %v, want %v\n", tc.input, got, tc.want)
+		}
+	}
+}
