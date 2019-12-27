8b439ed9ff101a2d33b5d41629d2585b638b56df
John.Lockwood <johnwlockwood@gmail.com>
2019-12-27 13:39:33 -0800

    Add stub for sort colors problem

---
+++ numbers/colors/sort/sort_test.go
@@ -0,0 +1,27 @@
+package sort
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	type test struct {
+		input []int
+		want  []int
+	}
+
+	tests := []test{
+		{
+			input: []int{2, 0, 2, 1, 1, 0},
+			want:  []int{0, 0, 1, 1, 2, 2},
+		},
+	}
+	for _, tc := range tests {
+		inputStr := fmt.Sprintf("%v", tc.input)
+		if sortColors(tc.input); !reflect.DeepEqual(tc.input, tc.want) {
+			t.Errorf("%v got %v, want %v\n", inputStr, tc.input, tc.want)
+		}
+	}
+}
