590f5cad260136daf19a609f3c7a8a1d64939611
John Lockwood <john@Johns-MacBook-Pro.local>
2019-12-28 15:19:11 -0800

    Add more tests

--- strings/common/common_test.go
+++ strings/common/common_test.go
@@ -15,9 +15,9 @@ func TestFindContiguousHistory(t *testing.T) {
 	user0 := []string{"/start", "/pink", "/register", "/orange", "/red", "a"}
 	user1 := []string{"/start", "/green", "/blue", "/pink", "/register", "/orange", "/one/two"}
 	user2 := []string{"a", "/one", "/two"}
-	//   user3 := []string {"/red", "/orange", "/yellow", "/green", "/blue", "/purple", "/white", "/amber", "/HotRodPink", "/BritishRacingGreen"}
-	//   user4 := []string {"/red", "/orange", "/amber", "/random", "/green", "/blue", "/purple", "/white", "/lavender", "/HotRodPink", "/BritishRacingGreen"}
-	//   user5 := []string {"a"}
+	user3 := []string{"/red", "/orange", "/yellow", "/green", "/blue", "/purple", "/white", "/amber", "/HotRodPink", "/BritishRacingGreen"}
+	user4 := []string{"/red", "/orange", "/amber", "/random", "/green", "/blue", "/purple", "/white", "/lavender", "/HotRodPink", "/BritishRacingGreen"}
+	user5 := []string{"a"}
 
 	tests := []test{
 		{
@@ -29,6 +29,24 @@ func TestFindContiguousHistory(t *testing.T) {
 		{
 			name:   "user0, user2",
 			inputA: user0,
+			inputB: user2,
+			want:   []string{"a"},
+		},
+		{
+			name:   "user2, user0",
+			inputA: user2,
+			inputB: user0,
+			want:   []string{"a"},
+		},
+		{
+			name:   "user3, user4",
+			inputA: user3,
+			inputB: user4,
+			want:   []string{"/green", "/blue", "/purple", "/white"},
+		},
+		{
+			name:   "user5, user2",
+			inputA: user5,
 			inputB: user2,
 			want:   []string{"a"},
 		},
