87ed9c4465cd1b4cbf47c2ce7e6d5359d6ce0bf7
John.Lockwood <johnwlockwood@gmail.com>
2019-12-27 16:19:25 -0800

    Add sort colors solution

--- numbers/colors/sort/sort.go
+++ numbers/colors/sort/sort.go
@@ -1,5 +1,15 @@
 package sort
 
 func sortColors(nums []int) {
-
+	colorCounts := make([]int, 3)
+	for i := 0; i < len(nums); i++ {
+		colorCounts[nums[i]]++
+	}
+	p := 0
+	for i := 0; i < 3; i++ {
+		for j := 0; j < colorCounts[i]; j++ {
+			nums[p] = i
+			p++
+		}
+	}
 }
