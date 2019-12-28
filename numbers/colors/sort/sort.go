baee3e2a385837df92b10f488ce10354c442a5d8
John.Lockwood <johnwlockwood@gmail.com>
2019-12-27 17:23:39 -0800

    Add one pass solution

--- numbers/colors/sort/sort.go
+++ numbers/colors/sort/sort.go
@@ -13,3 +13,25 @@ func sortColors(nums []int) {
 		}
 	}
 }
+
+// based on the Dutch National Flag Problem
+func sortColorsPointers(nums []int) {
+	// pointer to the rightmost boundary of 0s
+	p0 := 0
+	// pointer to the leftmost boundary of 2s
+	p2 := len(nums) - 1
+	// index of current element
+	curr := 0
+	for curr <= p2 {
+		if nums[curr] == 0 {
+			nums[curr], nums[p0] = nums[p0], nums[curr]
+			p0++
+			curr++
+		} else if nums[curr] == 2 {
+			nums[curr], nums[p2] = nums[p2], nums[curr]
+			p2--
+		} else {
+			curr++
+		}
+	}
+}
--- numbers/colors/sort/sort_test.go
+++ numbers/colors/sort/sort_test.go
@@ -17,10 +17,38 @@ func TestSort(t *testing.T) {
 			input: []int{2, 0, 2, 1, 1, 0},
 			want:  []int{0, 0, 1, 1, 2, 2},
 		},
+		{
+			input: []int{2, 0, 1},
+			want:  []int{0, 1, 2},
+		},
 	}
 	for _, tc := range tests {
 		inputStr := fmt.Sprintf("%v", tc.input)
 		if sortColors(tc.input); !reflect.DeepEqual(tc.input, tc.want) {
+			t.Errorf("%v got %v, want %v\n", inputStr, tc.input, tc.want)
+		}
+	}
+}
+
+func TestSortPointers(t *testing.T) {
+	type test struct {
+		input []int
+		want  []int
+	}
+
+	tests := []test{
+		{
+			input: []int{2, 0, 2, 1, 1, 0},
+			want:  []int{0, 0, 1, 1, 2, 2},
+		},
+		{
+			input: []int{2, 0, 1},
+			want:  []int{0, 1, 2},
+		},
+	}
+	for _, tc := range tests {
+		inputStr := fmt.Sprintf("%v", tc.input)
+		if sortColorsPointers(tc.input); !reflect.DeepEqual(tc.input, tc.want) {
 			t.Errorf("%v got %v, want %v\n", inputStr, tc.input, tc.want)
 		}
 	}
