57ca222b0e5599b1e3574376aef8ace7112f24e3
John Lockwood <john@Johns-MacBook-Pro.local>
2019-12-29 17:06:20 -0800

    Add iterative approach to inorder

--- bintree/bintree.go
+++ bintree/bintree.go
@@ -49,6 +49,64 @@ func InorderTraversal(root *TreeNode) []int {
 	return vals
 }
 
+// InorderTraversalIterative traverses a binary tree inorder using an iterative approach
+func InorderTraversalIterative(root *TreeNode) []int {
+
+	// uses a stack for the nodes with which we are not done with.
+	// Prime the stack with the root and a state struct
+	// while there are nodes on the stack
+	// for a node and it's state
+	// use the state to determine if the current node
+	// should:
+	// go back on the stack and add the left node;
+	// the value from the node needs to be added to the vals;
+	// or the right node needs to be added to the stack
+
+	if root == nil {
+		return []int{}
+	}
+	type inorderState struct {
+		left bool
+		node bool
+	}
+	states := make([]inorderState, 0)
+	nodes := make([]*TreeNode, 0)
+	vals := make([]int, 0)
+	// traverse left until left is nil
+	// append leaf val
+	var curr *TreeNode
+	nodes = append(nodes, root)
+	states = append(states, inorderState{})
+
+	for len(nodes) > 0 {
+		curr = nodes[len(nodes)-1]
+		s := states[len(states)-1]
+		nodes = nodes[:len(nodes)-1]
+		states = states[:len(states)-1]
+		if !s.left {
+			s.left = true
+			nodes = append(nodes, curr)
+			states = append(states, s)
+			if curr.Left != nil {
+				nodes = append(nodes, curr.Left)
+				states = append(states, inorderState{})
+			}
+		} else if !s.node {
+			vals = append(vals, curr.Val)
+			s.node = true
+			nodes = append(nodes, curr)
+			states = append(states, s)
+		} else {
+			if curr.Right != nil {
+				nodes = append(nodes, curr.Right)
+				states = append(states, inorderState{})
+			}
+		}
+	}
+
+	return vals
+}
+
 // contest/weekly-contest-169/problems/all-elements-in-two-binary-search-trees/
 // from contest 169
 func getAllElements(root1 *TreeNode, root2 *TreeNode) []int {
