35feeff7bdefa7212baaae3e52b22b835e7b2c78
John Lockwood <john@Johns-MacBook-Pro.local>
2019-12-31 15:55:09 -0800

    Use map to lookup index of val in inorder

--- bintree/construct.go
+++ bintree/construct.go
@@ -11,11 +11,15 @@ func buildTree(inorder []int, postorder []int) *TreeNode {
 	if len(inorder) <= 0 || len(postorder) <= 0 {
 		return nil
 	}
-	return helper(inorder, postorder, 0, len(inorder)-1, 0, len(postorder)-1, 0)
+	inOrderIndexMap := make(map[int]int, len(inorder))
+	for i, v := range inorder {
+		inOrderIndexMap[v] = i
+	}
+	return helper(inorder, postorder, inOrderIndexMap, 0, len(inorder)-1, 0, len(postorder)-1, 0)
 }
 
-func helper(inorder, postorder []int, inLeft, inRight, postLeft, postRight, indent int) *TreeNode {
-	if inRight < inLeft {
+func helper(inorder, postorder []int, inOrderIndexMap map[int]int, inLeft, inRight, postLeft, postRight, indent int) *TreeNode {
+	if inLeft > inRight {
 		return nil
 	}
 	fmt.Printf("%sinorder:   %v\n", strings.Repeat("\t", indent), inorder[inLeft:inRight+1])
@@ -29,22 +33,16 @@ func helper(inorder, postorder []int, inLeft, inRight, postLeft, postRight, inde
 	fmt.Printf("%sVal: %d\n", strings.Repeat("\t", indent), rootVal)
 
 	// find the root in inorder
-	rootInOrderIndex := inLeft
-	for rootInOrderIndex <= inRight {
-		if inorder[rootInOrderIndex] == rootVal {
-			break
-		}
-		rootInOrderIndex++
-	}
+	rootInOrderIndex := inOrderIndexMap[rootVal]
 	// Build Left Node
 	fmt.Printf("%sBuild Left for root %d\n", strings.Repeat("\t", indent), rootVal)
 	lenLeft := rootInOrderIndex - inLeft
 	lenRight := inRight - rootInOrderIndex
-	root.Left = helper(inorder, postorder, inLeft, inLeft+lenLeft-1, postLeft, postRight-1-lenRight, indent+1)
+	root.Left = helper(inorder, postorder, inOrderIndexMap, inLeft, inLeft+lenLeft-1, postLeft, postRight-1-lenRight, indent+1)
 	fmt.Println("")
 	// Build Right Node
 	fmt.Printf("%sBuild Right for root %d\n", strings.Repeat("\t", indent), rootVal)
-	root.Right = helper(inorder, postorder, rootInOrderIndex+1, inRight, postRight-lenRight, postRight-1, indent+1)
+	root.Right = helper(inorder, postorder, inOrderIndexMap, rootInOrderIndex+1, inRight, postRight-lenRight, postRight-1, indent+1)
 	fmt.Println("")
 
 	return root
