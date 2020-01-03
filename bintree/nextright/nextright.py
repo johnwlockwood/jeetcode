8d5daa5684e1705f148df52b623bd38ae8a3316b
John Lockwood <john@Johns-MacBook-Pro.local>
2020-01-02 17:29:18 -0800

    Define Node

--- bintree/nextright/nextright.py
+++ bintree/nextright/nextright.py
@@ -8,6 +8,14 @@ class Node:
         self.right = right
         self.next = next
 """
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
 import queue
 
 class Solution:
