12fff48f6361bd3a8ca3b34e9c0e425e477ad78c
John Lockwood <john@Johns-MacBook-Pro.local>
2020-01-05 19:32:09 -0800

    Implement sort interface

--- numbers/largest/largest.go
+++ numbers/largest/largest.go
@@ -32,22 +32,29 @@ func largestNumber(nums []int) string {
 	return l
 }
 
-func largestNumber2(nums []int) string {
-	sort.Slice(nums, func(i, j int) bool {
-		is := strconv.Itoa(nums[i])
-		js := strconv.Itoa(nums[j])
-		ij, err := strconv.ParseInt(is+js, 10, 64)
-		if err != nil {
-			return false
-		}
-		ji, err := strconv.ParseInt(js+is, 10, 64)
-		if err != nil {
-			return false
-		}
-		return ij > ji
-	})
+type byLargestCombo []int
+
+func (a byLargestCombo) Len() int      { return len(a) }
+func (a byLargestCombo) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byLargestCombo) Less(i, j int) bool {
+	is := strconv.Itoa(a[i])
+	js := strconv.Itoa(a[j])
+	ij, err := strconv.ParseInt(is+js, 10, 64)
+	if err != nil {
+		return false
+	}
+	ji, err := strconv.ParseInt(js+is, 10, 64)
+	if err != nil {
+		return false
+	}
+	return ij > ji
+}
+
+func largestNumberB(nums []int) string {
+	a := byLargestCombo(nums)
+	sort.Sort(a)
 	b := new(strings.Builder)
-	for _, n := range nums {
+	for _, n := range a {
 		fmt.Fprint(b, strconv.FormatInt(int64(n), 10))
 	}
 	l := strings.TrimLeft(b.String(), "0")
--- numbers/largest/largest_test.go
+++ numbers/largest/largest_test.go
@@ -35,11 +35,42 @@ func TestLargestNumber(t *testing.T) {
 	}
 }
 
+func TestLargestNumberB(t *testing.T) {
+	type test struct {
+		input []int
+		want  string
+	}
+	tests := []test{
+		{
+			input: []int{999999998, 999999997, 999999999},
+			want:  "999999999999999998999999997",
+		},
+		{
+			input: []int{0, 0},
+			want:  "0",
+		},
+		{
+			input: []int{10, 2},
+			want:  "210",
+		},
+		{
+			input: []int{3, 30, 34, 5, 9},
+			want:  "9534330",
+		},
+	}
+
+	for _, tc := range tests {
+		if got := largestNumberB(tc.input); got != tc.want {
+			t.Errorf("for input %v: got %s, want %s", tc.input, got, tc.want)
+		}
+	}
+}
+
 // BenchmarkLargestNumber/3,_30,_34,_5,_9-16         	 1616294	       697 ns/op	     144 B/op	      15 allocs/op
 // BenchmarkLargestNumber/3,_30,_34,_5,_9,_0,_0,_999999997,_999999999-16         	  767232	      1640 ns/op	     512 B/op	      29 allocs/op
 // BenchmarkLargestNumber/range_3_-_66-16                                        	   53311	     23418 ns/op	    4384 B/op	     408 allocs/op
 // PASS
-func BenchmarkLargestNumber(b *testing.B) {
+func BenchmarkLargestNumberA(b *testing.B) {
 	type bench struct {
 		name  string
 		input []int
@@ -68,6 +99,40 @@ func BenchmarkLargestNumber(b *testing.B) {
 		b.Run(bc.name, func(b *testing.B) {
 			for n := 0; n < b.N; n++ {
 				_ = largestNumber(bc.input)
+			}
+		})
+	}
+}
+
+func BenchmarkLargestNumbeB(b *testing.B) {
+	type bench struct {
+		name  string
+		input []int
+	}
+
+	a := new([50]int)
+	for i := 0; i < 50; i++ {
+		a[i] = i + 30
+	}
+
+	benches := []bench{
+		{
+			name:  "3, 30, 34, 5, 9",
+			input: []int{3, 30, 34, 5, 9},
+		},
+		{
+			name:  "3, 30, 34, 5, 9, 0, 0, 999999997, 999999999",
+			input: []int{3, 30, 34, 5, 9, 999999997, 999999999},
+		},
+		{
+			name:  "range 3 - 66",
+			input: a[:],
+		},
+	}
+	for _, bc := range benches {
+		b.Run(bc.name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				_ = largestNumberB(bc.input)
 			}
 		})
 	}
