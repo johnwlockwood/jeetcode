1dc95ef67659703fc930324c70ce572493a36834
John Lockwood <john@Johns-MacBook-Pro.local>
2020-01-05 21:32:51 -0800

    Avoid converting to strings while sorting

--- numbers/largest/largest.go
+++ numbers/largest/largest.go
@@ -32,24 +32,44 @@ func largestNumber(nums []int) string {
 	return l
 }
 
+func countDigits(i int) int {
+	count := 0
+	if i == 0 {
+		return 1
+	}
+	for i != 0 {
+		i /= 10
+		count++
+	}
+	return count
+}
+
 type byLargestCombo []int
 
 func (a byLargestCombo) Len() int      { return len(a) }
 func (a byLargestCombo) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a byLargestCombo) Less(i, j int) bool {
-	is := strconv.Itoa(a[i])
-	js := strconv.Itoa(a[j])
-	ij, err := strconv.ParseInt(is+js, 10, 64)
-	if err != nil {
-		return false
+	ic := countDigits(a[i])
+	jc := countDigits(a[j])
+
+	ij := a[i]
+	for i := 0; i < jc; i++ {
+		ij *= 10
 	}
-	ji, err := strconv.ParseInt(js+is, 10, 64)
-	if err != nil {
-		return false
+	ij += a[j]
+
+	ji := a[j]
+	for i := 0; i < ic; i++ {
+		ji *= 10
 	}
+	ji += a[i]
+
 	return ij > ji
 }
 
+// BenchmarkLargestNumbeB/3,_30,_34,_5,_9-16         	 1983177	       591 ns/op	     184 B/op	       9 allocs/op
+// BenchmarkLargestNumbeB/3,_30,_34,_5,_9,_0,_0,_999999997,_999999999-16         	 1000000	      1013 ns/op	     288 B/op	      14 allocs/op
+// BenchmarkLargestNumbeB/range_3_-_66-16                                        	  138556	      7410 ns/op	    1144 B/op	      58 allocs/op
 func largestNumberB(nums []int) string {
 	a := byLargestCombo(nums)
 	sort.Sort(a)
