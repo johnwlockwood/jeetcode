6f04b7ac71dbaf7abc2013d0f379eb5cfbeffde6
John Lockwood <john@Johns-MacBook-Pro.local>
2020-01-05 18:12:33 -0800

    Add benchmark

--- numbers/largest/largest.go
+++ numbers/largest/largest.go
@@ -1,6 +1,7 @@
 package largest
 
 import (
+	"fmt"
 	"sort"
 	"strconv"
 	"strings"
@@ -25,6 +26,31 @@ func largestNumber(nums []int) string {
 		l += strconv.FormatInt(int64(n), 10)
 	}
 	l = strings.TrimLeft(l, "0")
+	if len(l) == 0 {
+		return "0"
+	}
+	return l
+}
+
+func largestNumber2(nums []int) string {
+	sort.Slice(nums, func(i, j int) bool {
+		is := strconv.Itoa(nums[i])
+		js := strconv.Itoa(nums[j])
+		ij, err := strconv.ParseInt(is+js, 10, 64)
+		if err != nil {
+			return false
+		}
+		ji, err := strconv.ParseInt(js+is, 10, 64)
+		if err != nil {
+			return false
+		}
+		return ij > ji
+	})
+	b := new(strings.Builder)
+	for _, n := range nums {
+		fmt.Fprint(b, strconv.FormatInt(int64(n), 10))
+	}
+	l := strings.TrimLeft(b.String(), "0")
 	if len(l) == 0 {
 		return "0"
 	}
--- numbers/largest/largest_test.go
+++ numbers/largest/largest_test.go
@@ -34,3 +34,41 @@ func TestLargestNumber(t *testing.T) {
 		}
 	}
 }
+
+// BenchmarkLargestNumber/3,_30,_34,_5,_9-16         	 1616294	       697 ns/op	     144 B/op	      15 allocs/op
+// BenchmarkLargestNumber/3,_30,_34,_5,_9,_0,_0,_999999997,_999999999-16         	  767232	      1640 ns/op	     512 B/op	      29 allocs/op
+// BenchmarkLargestNumber/range_3_-_66-16                                        	   53311	     23418 ns/op	    4384 B/op	     408 allocs/op
+// PASS
+func BenchmarkLargestNumber(b *testing.B) {
+	type bench struct {
+		name  string
+		input []int
+	}
+
+	a := new([50]int)
+	for i := 0; i < 50; i++ {
+		a[i] = i + 30
+	}
+
+	benches := []bench{
+		{
+			name:  "3, 30, 34, 5, 9",
+			input: []int{3, 30, 34, 5, 9},
+		},
+		{
+			name:  "3, 30, 34, 5, 9, 0, 0, 999999997, 999999999",
+			input: []int{3, 30, 34, 5, 9, 999999997, 999999999},
+		},
+		{
+			name:  "range 3 - 66",
+			input: a[:],
+		},
+	}
+	for _, bc := range benches {
+		b.Run(bc.name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				_ = largestNumber(bc.input)
+			}
+		})
+	}
+}
