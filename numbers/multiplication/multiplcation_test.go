7ce17c6fe32c81b38ac006ed843018814c8b200f
John Lockwood <john@Johns-MacBook-Pro.local>
2020-01-07 13:33:09 -0800

    Failing Karatsuba

--- numbers/multiplication/multiplcation_test.go
+++ numbers/multiplication/multiplcation_test.go
@@ -1,6 +1,9 @@
 package multiplication
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMultiplication(t *testing.T) {
 	type test struct {
@@ -10,20 +13,93 @@ func TestMultiplication(t *testing.T) {
 	}
 
 	tests := []test{
+		// {
+		// 	inputA: "1",
+		// 	inputB: "2",
+		// 	want:   "2",
+		// },
+		// {
+		// 	inputA: "10",
+		// 	inputB: "20",
+		// 	want:   "200",
+		// },
+		{
+			inputA: "10",
+			inputB: "55",
+			want:   "550",
+		},
+		// {
+		// 	inputA: "20",
+		// 	inputB: "15",
+		// 	want:   "300",
+		// },
+		// {
+		// 	inputA: "1000",
+		// 	inputB: "5555",
+		// 	want:   "5555000",
+		// },
+		// {
+		// 	inputA: "5678",
+		// 	inputB: "1234",
+		// 	want:   "7006652",
+		// },
+	}
+
+	for _, tc := range tests {
+		fmt.Println("")
+		if got := multiply(tc.inputA, tc.inputB, 0); got != tc.want {
+			t.Errorf("for %s x %s, got %s, want %s", tc.inputA, tc.inputB, got, tc.want)
+		}
+	}
+}
+
+func TestAddition(t *testing.T) {
+	type test struct {
+		inputA string
+		inputB string
+		want   string
+	}
+
+	tests := []test{
+		{
+			inputA: "1",
+			inputB: "1",
+			want:   "2",
+		},
+		{
+			inputA: "10",
+			inputB: "1",
+			want:   "11",
+		},
+		{
+			inputA: "5",
+			inputB: "6",
+			want:   "11",
+		},
+		{
+			inputA: "26",
+			inputB: "16",
+			want:   "42",
+		},
+		{
+			inputA: "26",
+			inputB: "-16",
+			want:   "10",
+		},
+		{
+			inputA: "-26",
+			inputB: "16",
+			want:   "-10",
+		},
 		{
 			inputA: "1000",
 			inputB: "5555",
-			want:   "5555000",
-		},
-		{
-			inputA: "5678",
-			inputB: "1234",
-			want:   "7006652",
+			want:   "6555",
 		},
 	}
 
 	for _, tc := range tests {
-		if got := multiply(tc.inputA, tc.inputA); got != tc.want {
+		if got := add(tc.inputA, tc.inputB); got != tc.want {
 			t.Errorf("for %s x %s, got %s, want %s", tc.inputA, tc.inputB, got, tc.want)
 		}
 	}
--- numbers/multiplication/multiplication.go
+++ numbers/multiplication/multiplication.go
@@ -1,5 +1,104 @@
 package multiplication
 
-func multiply(a, b string) string {
-	return "0"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func multiply(x, y string, indent int) string {
+	if len(x) != len(y) {
+		if len(x) < len(y) {
+			x = strings.Repeat("0", len(y)-len(x)) + x
+		} else {
+			y = strings.Repeat("0", len(x)-len(y)) + y
+		}
+	}
+	fmt.Printf("%s%s x %s\n", strings.Repeat("\t", indent), x, y)
+	n := len(x)
+	if n == 1 {
+		a, _ := strconv.Atoi(x)
+		b, _ := strconv.Atoi(y)
+		total := strconv.Itoa(a * b)
+		fmt.Printf("%s= %s\n", strings.Repeat("\t", indent), total)
+		return total
+	}
+	a, b := x[:n/2], x[n/2:]
+	c, d := y[:n/2], y[n/2:]
+	fmt.Printf("%sa: %s, b: %s, c: %s, d: %s\n", strings.Repeat("\t", indent), a, b, c, d)
+	p := add(a, b)
+	q := add(c, d)
+	fmt.Printf("%sa:%s + b:%s = p: %s\n", strings.Repeat("\t", indent), a, b, p)
+	fmt.Printf("%sc:%s + d:%s = q:%s\n", strings.Repeat("\t", indent), c, d, q)
+	fmt.Printf("%sa x c\n", strings.Repeat("\t", indent))
+	ac := multiply(a, c, indent+1)
+	fmt.Printf("%sb x d\n", strings.Repeat("\t", indent))
+	bd := multiply(b, d, indent+1)
+	fmt.Printf("%sp x q\n", strings.Repeat("\t", indent))
+	pq := multiply(p, q, indent+1)
+
+	fmt.Printf("%spq: %s, ac: %s, bd: %s\n", strings.Repeat("\t", indent), pq, ac, bd)
+	adbc := add(add(pq, "-"+ac), "-"+bd)
+	// fmt.Printf("%sadbc: %s\n", strings.Repeat("\t", indent), adbc)
+	fmt.Printf("%sac:%s * 10**n: %s\n", strings.Repeat("\t", indent), ac, ac+strings.Repeat("0", n))
+	fmt.Printf("%sadbc:%s * 10**(n/2): %s\n", strings.Repeat("\t", indent), adbc, adbc+strings.Repeat("0", n/2))
+
+	ac10n := ac + strings.Repeat("0", n)
+	adbc10nD2 := adbc + strings.Repeat("0", n/2)
+	total := add(ac10n, adbc10nD2)
+	total = add(total, bd)
+	total = strings.TrimLeft(total, "0")
+	if len(total) == 0 {
+		total = "0"
+	}
+	fmt.Printf("%stotal: %s\n", strings.Repeat("\t", indent), total)
+	return total
+}
+
+func sign(x string) (string, string) {
+	if len(x) > 1 {
+		switch x[0] {
+		case '-':
+			return "-", x[1:]
+		case '+':
+			return "+", x[1:]
+		}
+	}
+	return "+", x
+}
+
+func add(x, y string) string {
+	signX, x := sign(x)
+	signY, y := sign(y)
+
+	if len(x) != len(y) {
+		if len(x) < len(y) {
+			x = strings.Repeat("0", len(y)-len(x)) + x
+		} else {
+			y = strings.Repeat("0", len(x)-len(y)) + y
+		}
+	}
+	n := len(x)
+	carry := 0
+	result := make([]byte, 0)
+	for i := n - 1; i >= 0; i-- {
+		a, _ := strconv.Atoi(string(signX + x[i:i+1]))
+		b, _ := strconv.Atoi(string(signY + y[i:i+1]))
+		p := a + b + carry
+		signPs, ps := sign(strconv.Itoa(p))
+		if signPs == "-" {
+			carry = -1
+		} else if len(ps) > 1 {
+			carry, _ = strconv.Atoi(ps[:1])
+			result = append([]byte{ps[1]}, result...)
+		} else {
+			carry = 0
+			result = append([]byte{ps[0]}, result...)
+		}
+	}
+	if carry != 0 {
+		cs := []byte(strconv.Itoa(carry))
+		result = append(cs, result...)
+	}
+	return string(result)
 }
