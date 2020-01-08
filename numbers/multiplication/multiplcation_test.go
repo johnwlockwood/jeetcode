c5a6db40aaa44f07de26d63af86593b40c99aec5
John Lockwood <john@Johns-MacBook-Pro.local>
2020-01-07 18:16:12 -0800

    Add working recursive large integer multiplication

--- numbers/multiplication/multiplcation_test.go
+++ numbers/multiplication/multiplcation_test.go
@@ -13,36 +13,99 @@ func TestMultiplication(t *testing.T) {
 	}
 
 	tests := []test{
-		// {
-		// 	inputA: "1",
-		// 	inputB: "2",
-		// 	want:   "2",
-		// },
-		// {
-		// 	inputA: "10",
-		// 	inputB: "20",
-		// 	want:   "200",
-		// },
+		{
+			inputA: "1",
+			inputB: "2",
+			want:   "2",
+		},
+		{
+			inputA: "10",
+			inputB: "20",
+			want:   "200",
+		},
 		{
 			inputA: "10",
 			inputB: "55",
 			want:   "550",
 		},
-		// {
-		// 	inputA: "20",
-		// 	inputB: "15",
-		// 	want:   "300",
-		// },
-		// {
-		// 	inputA: "1000",
-		// 	inputB: "5555",
-		// 	want:   "5555000",
-		// },
-		// {
-		// 	inputA: "5678",
-		// 	inputB: "1234",
-		// 	want:   "7006652",
-		// },
+		{
+			inputA: "20",
+			inputB: "15",
+			want:   "300",
+		},
+		{
+			inputA: "5555",
+			inputB: "1000",
+			want:   "5555000",
+		},
+		{
+			inputA: "1000",
+			inputB: "5555",
+			want:   "5555000",
+		},
+		{
+			inputA: "5678",
+			inputB: "1234",
+			want:   "7006652",
+		},
+		{
+			inputA: "3141592653589793238462643383279502884197169399375105820974944592",
+			inputB: "2718281828459045235360287471352662497757247093699959574966967627",
+			want:   "7006652",
+		},
+	}
+
+	for _, tc := range tests {
+		fmt.Println("")
+		if got := multiplyRecursive(tc.inputA, tc.inputB, 0); got != tc.want {
+			t.Errorf("for %s x %s, got %s, want %s", tc.inputA, tc.inputB, got, tc.want)
+		}
+	}
+}
+
+func TestKaratsubaMultiplication(t *testing.T) {
+	type test struct {
+		inputA string
+		inputB string
+		want   string
+	}
+
+	tests := []test{
+		{
+			inputA: "1",
+			inputB: "2",
+			want:   "2",
+		},
+		{
+			inputA: "10",
+			inputB: "20",
+			want:   "200",
+		},
+		{
+			inputA: "10",
+			inputB: "55",
+			want:   "550",
+		},
+		{
+			inputA: "20",
+			inputB: "15",
+			want:   "300",
+		},
+		{
+			inputA: "5555",
+			inputB: "1000",
+			want:   "5555000",
+		},
+		{
+			inputA: "1000",
+			inputB: "5555",
+			want:   "5555000",
+		},
+		{
+			inputA: "5678",
+			inputB: "1234",
+			want:   "7006652",
+		},
 	}
 
 	for _, tc := range tests {
@@ -61,6 +124,11 @@ func TestAddition(t *testing.T) {
 	}
 
 	tests := []test{
+		{
+			inputA: "10",
+			inputB: "-5",
+			want:   "5",
+		},
 		{
 			inputA: "1",
 			inputB: "1",
@@ -86,11 +154,16 @@ func TestAddition(t *testing.T) {
 			inputB: "-16",
 			want:   "10",
 		},
-		{
-			inputA: "-26",
-			inputB: "16",
-			want:   "-10",
-		},
+		// {
+		// 	inputA: "26",
+		// 	inputB: "-36",
+		// 	want:   "-10",
+		// },
+		// {
+		// 	inputA: "-26",
+		// 	inputB: "16",
+		// 	want:   "-10",
+		// },
 		{
 			inputA: "1000",
 			inputB: "5555",
--- numbers/multiplication/multiplication.go
+++ numbers/multiplication/multiplication.go
@@ -2,10 +2,13 @@ package multiplication
 
 import (
 	"fmt"
+	"math/big"
 	"strconv"
 	"strings"
 )
 
+// Karatsuba multiplication
+// TODO: make correct
 func multiply(x, y string, indent int) string {
 	if len(x) != len(y) {
 		if len(x) < len(y) {
@@ -23,36 +26,120 @@ func multiply(x, y string, indent int) string {
 		fmt.Printf("%s= %s\n", strings.Repeat("\t", indent), total)
 		return total
 	}
-	a, b := x[:n/2], x[n/2:]
-	c, d := y[:n/2], y[n/2:]
-	fmt.Printf("%sa: %s, b: %s, c: %s, d: %s\n", strings.Repeat("\t", indent), a, b, c, d)
-	p := add(a, b)
-	q := add(c, d)
-	fmt.Printf("%sa:%s + b:%s = p: %s\n", strings.Repeat("\t", indent), a, b, p)
+	a, _ := new(big.Int).SetString(x[:n/2], 10)
+	b, _ := new(big.Int).SetString(x[n/2:], 10)
+	c, _ := new(big.Int).SetString(y[:n/2], 10)
+	d, _ := new(big.Int).SetString(y[n/2:], 10)
+
+	p := big.NewInt(0).Add(a, b)
+
+	// p := add(a, b)
+
+	// q := add(c, d)
+	q := big.NewInt(0).Add(c, d)
+
+	fmt.Printf("%sa:%s + b:%s = p:%s\n", strings.Repeat("\t", indent), a, b, p)
 	fmt.Printf("%sc:%s + d:%s = q:%s\n", strings.Repeat("\t", indent), c, d, q)
-	fmt.Printf("%sa x c\n", strings.Repeat("\t", indent))
-	ac := multiply(a, c, indent+1)
-	fmt.Printf("%sb x d\n", strings.Repeat("\t", indent))
-	bd := multiply(b, d, indent+1)
-	fmt.Printf("%sp x q\n", strings.Repeat("\t", indent))
-	pq := multiply(p, q, indent+1)
+
+	ac := multiply(a.String(), c.String(), indent+1)
+	fmt.Printf("%sa x c = ", strings.Repeat("\t", indent))
+	fmt.Printf("%s\n", ac)
+
+	bd := multiply(b.String(), d.String(), indent+1)
+	fmt.Printf("%sb x d = ", strings.Repeat("\t", indent))
+	fmt.Printf("%s\n", bd)
+
+	pq := multiply(p.String(), q.String(), indent+1)
+	fmt.Printf("%sp x q = ", strings.Repeat("\t", indent))
+	fmt.Printf("%s\n", pq)
 
 	fmt.Printf("%spq: %s, ac: %s, bd: %s\n", strings.Repeat("\t", indent), pq, ac, bd)
-	adbc := add(add(pq, "-"+ac), "-"+bd)
+
+	fmt.Printf("%sadbc = %s - %s - %s\n", strings.Repeat("\t", indent), pq, ac, bd)
+
+	pqB, _ := new(big.Int).SetString(pq, 10)
+	acB, _ := new(big.Int).SetString(ac, 10)
+	adbc := big.NewInt(0).Sub(pqB, acB)
+	bdB, _ := new(big.Int).SetString(bd, 10)
+	adbc.Sub(adbc, bdB)
+	// adbc := add(pq, "-"+ac)
+	// adbc = add(adbc, "-"+bd)
 	// fmt.Printf("%sadbc: %s\n", strings.Repeat("\t", indent), adbc)
 	fmt.Printf("%sac:%s * 10**n: %s\n", strings.Repeat("\t", indent), ac, ac+strings.Repeat("0", n))
-	fmt.Printf("%sadbc:%s * 10**(n/2): %s\n", strings.Repeat("\t", indent), adbc, adbc+strings.Repeat("0", n/2))
+	fmt.Printf("%sadbc:%s * 10**(n/2): %s\n", strings.Repeat("\t", indent), adbc.String(), adbc.String()+strings.Repeat("0", n/2))
 
-	ac10n := ac + strings.Repeat("0", n)
-	adbc10nD2 := adbc + strings.Repeat("0", n/2)
-	total := add(ac10n, adbc10nD2)
-	total = add(total, bd)
-	total = strings.TrimLeft(total, "0")
-	if len(total) == 0 {
-		total = "0"
+	ac10n, _ := new(big.Int).SetString(acB.String()+strings.Repeat("0", n), 10)
+	adbc10nD2, _ := new(big.Int).SetString(adbc.String()+strings.Repeat("0", n/2), 10)
+
+	total := new(big.Int).Add(ac10n, adbc10nD2)
+	total.Add(total, bdB)
+	// total := add(ac10n, adbc10nD2)
+	// total = add(total, bd)
+	fmt.Printf("%s%s x %s totals to: %s\n", strings.Repeat("\t", indent), x, y, total.String())
+	return total.String()
+}
+
+func multiplyRecursive(x, y string, indent int) string {
+	if len(x) != len(y) {
+		if len(x) < len(y) {
+			x = strings.Repeat("0", len(y)-len(x)) + x
+		} else {
+			y = strings.Repeat("0", len(x)-len(y)) + y
+		}
 	}
-	fmt.Printf("%stotal: %s\n", strings.Repeat("\t", indent), total)
-	return total
+	fmt.Printf("%s%s x %s\n", strings.Repeat("\t", indent), x, y)
+	n := len(x)
+	if n == 1 {
+		a, _ := strconv.Atoi(x)
+		b, _ := strconv.Atoi(y)
+		total := strconv.Itoa(a * b)
+		fmt.Printf("%s= %s\n", strings.Repeat("\t", indent), total)
+		return total
+	}
+	a, _ := new(big.Int).SetString(x[:n/2], 10)
+	b, _ := new(big.Int).SetString(x[n/2:], 10)
+	c, _ := new(big.Int).SetString(y[:n/2], 10)
+	d, _ := new(big.Int).SetString(y[n/2:], 10)
+
+	p := big.NewInt(0).Add(a, b)
+
+	// p := add(a, b)
+
+	// q := add(c, d)
+	q := big.NewInt(0).Add(c, d)
+
+	fmt.Printf("%sa:%s + b:%s = p:%s\n", strings.Repeat("\t", indent), a, b, p)
+	fmt.Printf("%sc:%s + d:%s = q:%s\n", strings.Repeat("\t", indent), c, d, q)
+
+	ac := multiplyRecursive(a.String(), c.String(), indent+1)
+	fmt.Printf("%sa x c = ", strings.Repeat("\t", indent))
+	fmt.Printf("%s\n", ac)
+
+	ad := multiplyRecursive(a.String(), d.String(), indent+1)
+
+	bc := multiplyRecursive(b.String(), c.String(), indent+1)
+
+	bd := multiplyRecursive(b.String(), d.String(), indent+1)
+	fmt.Printf("%sb x d = ", strings.Repeat("\t", indent))
+
+	acB, _ := new(big.Int).SetString(ac, 10)
+	adB, _ := new(big.Int).SetString(ad, 10)
+	bcB, _ := new(big.Int).SetString(bc, 10)
+	bdB, _ := new(big.Int).SetString(bd, 10)
+
+	adbc := new(big.Int).Add(adB, bcB)
+	fmt.Printf("%sac:%s * 10**n: %s\n", strings.Repeat("\t", indent), ac, ac+strings.Repeat("0", n))
+	fmt.Printf("%sad+bc:%s * 10**(n/2): %s\n", strings.Repeat("\t", indent), adbc.String(), adbc.String()+strings.Repeat("0", n/2))
+
+	ac10n, _ := new(big.Int).SetString(acB.String()+strings.Repeat("0", n), 10)
+	adbc10nD2, _ := new(big.Int).SetString(adbc.String()+strings.Repeat("0", n/2), 10)
+
+	total := new(big.Int).Add(ac10n, adbc10nD2)
+	total.Add(total, bdB)
+	// total := add(ac10n, adbc10nD2)
+	// total = add(total, bd)
+	fmt.Printf("%stotal: %s\n", strings.Repeat("\t", indent), total.String())
+	return total.String()
 }
 
 func sign(x string) (string, string) {
@@ -67,6 +154,7 @@ func sign(x string) (string, string) {
 	return "+", x
 }
 
+// TODO: make work with values which would produce a negative result
 func add(x, y string) string {
 	signX, x := sign(x)
 	signY, y := sign(y)
@@ -82,12 +170,25 @@ func add(x, y string) string {
 	carry := 0
 	result := make([]byte, 0)
 	for i := n - 1; i >= 0; i-- {
+		signX := signX
+		signY := signY
 		a, _ := strconv.Atoi(string(signX + x[i:i+1]))
 		b, _ := strconv.Atoi(string(signY + y[i:i+1]))
-		p := a + b + carry
+		// ensure largest is on top
+		if a < b {
+			a, b = b, a
+			signX, signY = signY, signX
+		}
+		var p int
+		if signX == signY {
+			p = a + b
+		}
+
+		p = (carry + a) + b
 		signPs, ps := sign(strconv.Itoa(p))
-		if signPs == "-" {
+		if signPs == "-" && (carry+a) > b {
 			carry = -1
+			result = append([]byte{ps[0]}, result...)
 		} else if len(ps) > 1 {
 			carry, _ = strconv.Atoi(ps[:1])
 			result = append([]byte{ps[1]}, result...)
@@ -100,5 +201,14 @@ func add(x, y string) string {
 		cs := []byte(strconv.Itoa(carry))
 		result = append(cs, result...)
 	}
-	return string(result)
+	total := string(result)
+	signTotal, total := sign(total)
+	total = strings.TrimLeft(total, "0")
+	if len(total) == 0 {
+		return "0"
+	}
+	if signTotal == "-" {
+		total = signTotal + total
+	}
+	return total
 }
